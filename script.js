77ceb82974073cf7da59284012ae7dee81255b87
janisbarbals <janisbarbals520@gmail.com>
2022-01-17 15:04:04 +0200

    otrais

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1)
+const vardi=['Juris Kalniņš','Andris Bērziņš', 'Egīls Cūkmanis', 'Elizabete Kļaviņa', 'Amanda Lapiņa'];
+const balvas=['Telefons', 'Dators', 'Kamera', 'Automašīna', 'Televīzors'];
+const naudaKopa=1000000;
+let uzvartajuSkaits=5;
+let rand= Math.random()*10;
+rand= Math.floor(rand);
+console.log(vardi[rand]);
