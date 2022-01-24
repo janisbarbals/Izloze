449466e776188a1cdec20614865592632e8cc304
janisbarbals <janisbarbals520@gmail.com>
2022-01-24 15:04:02 +0200

    otra stunda

--- script.js
+++ script.js
@@ -2,6 +2,15 @@ const vardi=['Juris Kalniņš','Andris Bērziņš', 'Egīls Cūkmanis', 'Elizabe
 const balvas=['Telefons', 'Dators', 'Kamera', 'Automašīna', 'Televīzors'];
 const naudaKopa=1000000;
 let uzvartajuSkaits=5;
-let rand= Math.random()*5;
+let rindas= document.querySelector('.rindas');
+rindas.innerHTML=' ';
+for(let i=0;i<5;i++){
+let rand= Math.random()*vardi.length;
 rand= Math.floor(rand);
 console.log(vardi[rand]);
+}
+rindas.innerHTML=
+<tr>
+    <td>i</td>
+    <td>uzvarētājs</td>
+</tr>
