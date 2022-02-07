2da4a98c500ca0cb87c91334b556aff3bd860dd4
janisbarbals <janisbarbals520@gmail.com>
2022-02-07 15:06:28 +0200

    a

--- script.js
+++ script.js
@@ -1,16 +1,20 @@
 const vardi=['Juris Kalniņš','Andris Bērziņš', 'Egīls Cūkmanis', 'Elizabete Kļaviņa', 'Amanda Lapiņa'];
 const balvas=['Telefons', 'Dators', 'Kamera', 'Automašīna', 'Televīzors'];
-const naudaKopa=1000000;
-let uzvartajuSkaits=5;
-let rindas= document.querySelector('.rindas');
-rindas.innerHTML=' ';
-for(let i=0;i<5;i++){
-let rand= Math.random()*vardi.length;
-rand= Math.floor(rand);
-console.log(vardi[rand]);
-}
-rindas.innerHTML=
+const naudaKopa= 1000000; // kopeja summa 1 milj
+let uzvaretajuSkaits= 5;
+let rindas = document.querySelector('.rindas');
+rindas.innerHTML = ' ';
+for(let i=0; i < uzvaretajuSkaits; i++){
+let rand= Math.random() * vardi.length;
+rand= Math.floor(rand);// noapaļo uz leju
+let uzvaratejs = vardi[rand]; // izvada konsole nejausi izveletus vardus
+let laimests = balvas[rand];
+rindas.innerHTML+= `
 <tr>
-    <td>i</td>
-    <td>uzvarētājs</td>
-</tr>
+    <td>${i+1}</td>
+    <td>${uzvaratejs}</td>
+    <td>${laimests}</td>
+
+</tr>`
+
+}
